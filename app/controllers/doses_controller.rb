15a3b33b1c918a222c17b664c5f8c54693d06310
Thomas Chen <thomas.arsenal.chen@gmail.com>
2018-11-15 17:33:18 +0800

    done back

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,32 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if  @dose.save
+      redirect_to cocktail_path(@cocktail)
+      flash[:notice] = "Successfully created"
+    else
+      render :new
+      flash[:alert] = "Check your must-complete blocks"
+    end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path    # DELETE /restaurantss/:id
+  end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
